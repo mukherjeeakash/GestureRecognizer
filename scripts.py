c66134921c344cd175a9b3d0f571a59fa5249c0f
Rishu Bagga <rbagga@Rishus-MacBook-Pro.local>
2018-10-21 02:43:52 -0500

    updated script

--- scripts.py
+++ scripts.py
@@ -7,11 +7,11 @@ graph_def = tf.GraphDef()
 labels = ["0","1","2"]
 
 # Import the TF graph
-with tf.gfile.FastGFile(filename, 'rb') as f:
+with tf.gfile.FastGFile("model.pb", 'rb') as f:
     graph_def.ParseFromString(f.read())
     tf.import_graph_def(graph_def, name='')
 
 # Create a list of labels.
-with open(labels_filename, 'rt') as lf:
+with open("labels.txt", 'rt') as lf:
     for l in lf:
         labels.append(l.strip())
